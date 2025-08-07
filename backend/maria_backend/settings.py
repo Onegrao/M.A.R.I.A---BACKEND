70e83e9ec76353ef8f794728ccef2a9883e2d9a3
Andre <svarturinvest@gmail.com>
2025-08-06 20:30:18 -0400

    Iniciado models para preenchimento do DB a ser usado no login e cadastro de maquinas

--- backend/maria_backend/settings.py
+++ backend/maria_backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+#Camada responsavel por fazer 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
 ]
 
 ROOT_URLCONF = 'maria_backend.urls'
