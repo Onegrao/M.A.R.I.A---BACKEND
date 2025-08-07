d1fb15ef7d924e222aa744a672675dcf6c7e0e74
Andre <svarturinvest@gmail.com>
2025-08-06 20:49:58 -0400

    Adicionado serializers para comunicação entre requisições e bancos, views para funções de retorno de resposta

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Maquina,Usuario
+from .serializers import MaquinaSerializer,UsuarioSerializer
 
 # Create your views here.
+
+class MaquinaViewSet(viewsets.ModelViewSet):
+    queryset = Maquina.objects.all()
+    serializer_class = MaquinaSerializer
+
+class UsuarioViewSet(viewsets.ModelViewset):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
