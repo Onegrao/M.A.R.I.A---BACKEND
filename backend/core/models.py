79fa737237c2425cefbdc770a17458b855f05dbb
Andre <svarturinvest@gmail.com>
2025-08-06 20:30:18 -0400

    Iniciado models para preenchimento do DB a ser usado no login e cadastro de maquinas

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class Maquina(models.Model):
+    setor = models.CharField(max_length=100)
+    cod_serie = models.IntegerField(unique=True)
+    funcao = models.CharField(max_length = 100)
+    marca = models.CharField(max_length = 100)
+    nome = models.CharField(max_length = 100)
+    apelido = models.CharField(max_length=100, blank=True, null=True)
+    data__entrada = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+class Usuario(AbstractUser):
+    email = models.EmailField(unique=True)
+    cargo = models.CharField(max_length=100, blank=True, null=True)
+    empresa = models.CharField(max_length=100, blank=True, null=True)
+    
+    def __str__(self):
+        return self.email
