f9dc8308889948bcf480ed9544270a491471fd58
lais.carvalho <laiscarvalho082@gmail.com>
2025-08-11 16:37:13 -0400

    arquivo com a senha do usuário e adicionado na url tokens JWT

--- backend/maria_backend/urls.py
+++ backend/maria_backend/urls.py
@@ -1,8 +1,14 @@
-
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')), #nome do app
+    # Gera os tokens JWT (crachás de autenticação) necessários para um login real
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
