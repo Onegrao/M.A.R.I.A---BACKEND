5ebeb95dddb3e57e72db838b79a6d8307a1629b9
Lais Carvalho <laiscarvalho082@gmail.com>
2025-08-12 20:44:46 -0400

    settings atualizado

--- backend/maria_backend/settings.py
+++ backend/maria_backend/settings.py
@@ -10,11 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -80,11 +84,11 @@ WSGI_APPLICATION = 'maria_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_maquinas',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST', 'localhost'),     # Adicionado valor padrão
+        'PORT': int(os.getenv('DATABASE_PORT', '5432')) # Corrigido com valor padrão
     }
 }
 
